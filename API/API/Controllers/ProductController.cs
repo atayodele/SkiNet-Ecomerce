2a1ff3a069c9cff1cf7c7ee478c9d24e10f249da
Timilehin Ayeni <Timilehin Ayeni@VGG-LT-439>
2020-12-11 08:30:02 +0100

    first commit

---
+++ API/API/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _repo;
+
+        public ProductController(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return Ok(await _repo.GetProducts());
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _repo.GetProductById(id);
+        }
+    }
+}
